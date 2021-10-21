27a923273076b8b67c4febb318d0bc9725f508f0
dshuhart <55543546+dshuhart@users.noreply.github.com>
2021-10-21 13:18:12 -0400

    added if statments to modeDisplay.py

--- initial_dev/mode-selection-test/modeDisplay.py
+++ initial_dev/mode-selection-test/modeDisplay.py
@@ -1,25 +1,32 @@
 import enum
 
-class modeType (enum.Enum):
+class modeType (enum.Enum):     #creating enumerations with class
     Initialization = 1
     Profile_Select = 2
-    #Training_Mode = 3
-    #Live_Mode = 4
-    #AI_Assist_Mode = 5
-    #game_Mode = 6
+    Training_Mode = 3
+    Live_Mode = 4
+    AI_Assist_Mode = 5
+    Game_Mode = 6
+#Tests
 print("The enum name is: ", end="")
-print(modeType.Initialization.name)
+print(modeType.Initialization.name)      #print out name of enum
 
 print("The enum name is: ", end="")
 print(modeType.Profile_Select.name)
 print("hello chrsi")
+#end tests
 
-#psuedocode for functions (for reference not actual code)
-#if startup:
-    #print(modeType.Initialization.name)
+for currentMode in (modeType):
+    if currentMode == modeType.Initialization:
+        print(modeType.Initialization.name)
+    elif currentMode == modeType.Profile_Select:
+        print(modeType.Profile_Select.name)
+    elif currentMode == modeType.Training_Mode:
+        print(modeType.Training_Mode.name)
+    elif currentMode == modeType.Live_Mode:
+        print(modeType.Live_Mode.name)
+    elif currentMode == modeType.AI_Assist_Mode:
+        print(modeType.AI_Assist_Mode.name)
+    else:
+        print(modeType.Game_Mode.name)
 
-#if profSel:
-    #print(modeType.Profile_Select.name)
-
-#if training
-    #print(modeType.Training_Mode.name)
