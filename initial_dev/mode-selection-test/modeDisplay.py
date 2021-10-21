a33880ba5fde412db20669dd36aed4c31914606c
dshuhart <55543546+dshuhart@users.noreply.github.com>
2021-10-21 14:34:20 -0400

    no u in color

--- initial_dev/mode-selection-test/modeDisplay.py
+++ initial_dev/mode-selection-test/modeDisplay.py
@@ -1,32 +1,36 @@
 import enum
 
 class modeType (enum.Enum):     #creating enumerations with class
-    Initialization = 1
+    Initializing = 1
     Profile_Select = 2
     Training_Mode = 3
     Live_Mode = 4
-    AI_Assist_Mode = 5
-    Game_Mode = 6
+    #AI_Assist_Mode = 5
+    #Game_Mode = 6
+
+currentMode = modeType.Live_Mode
+
+#for currentMode in (modeType):
+if currentMode == modeType.Initializing:
+    print(modeType.Initialization.name)
+elif currentMode == modeType.Profile_Select:
+    print(modeType.Profile_Select.name)
+elif currentMode == modeType.Training_Mode:
+    print(modeType.Training_Mode.name)
+elif currentMode == modeType.Live_Mode:
+    print(modeType.Live_Mode.name)
+#elif currentMode == modeType.AI_Assist_Mode:
+    #print(modeType.AI_Assist_Mode.name)
+#else:
+    #print(modeType.Game_Mode.name)
+
+
+
 #Tests
-print("The enum name is: ", end="")
-print(modeType.Initialization.name)      #print out name of enum
+#print("The enum name is: ", end="")
+#print(modeType.Initialization.name)      #print out name of enum
 
-print("The enum name is: ", end="")
-print(modeType.Profile_Select.name)
-print("hello chrsi")
+#print("The enum name is: ", end="")
+#print(modeType.Profile_Select.name)
+#print("hello chrsi")
 #end tests
-
-for currentMode in (modeType):
-    if currentMode == modeType.Initialization:
-        print(modeType.Initialization.name)
-    elif currentMode == modeType.Profile_Select:
-        print(modeType.Profile_Select.name)
-    elif currentMode == modeType.Training_Mode:
-        print(modeType.Training_Mode.name)
-    elif currentMode == modeType.Live_Mode:
-        print(modeType.Live_Mode.name)
-    elif currentMode == modeType.AI_Assist_Mode:
-        print(modeType.AI_Assist_Mode.name)
-    else:
-        print(modeType.Game_Mode.name)
-
---
+++ initial_dev/mode-selection-test/oled1.py
@@ -0,0 +1,47 @@
+import time
+import os
+
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+#pin configuration,on the OLED this pin isnt used
+RST = None
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=1, gpio=1)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image
+draw = ImageDraw.Draw(image)
+
+#Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+#Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+paddling = -2
+top = paddling
+bottom = height-paddling
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Load default font.
+font = ImageFont.load_default()
+
