abbc1727060f34ef5e0b40b5d3bef91f4648394b
dshuhart <55543546+dshuhart@users.noreply.github.com>
2021-10-29 10:52:08 -0400

    Pray to the lord almighty that it works

--- initial_dev/mode-selection-test/main.py
+++ initial_dev/mode-selection-test/main.py
@@ -13,32 +13,42 @@ class modeType (enum.Enum):     # creating enumerations with class
     # Game_Mode = 6
 
 
-currentMode = modeType.Initializing               # set default mode to Initialization
-Draw.printToOled()
+#oledDriver = Draw()
+
+#Draw.printToOled()
     
    
-oledDriver = Draw()
+#oledDriver = Draw()
 
 
 def main():
+    previousMode = None
+    currentMode = modeType.Initializing      # set default mode to Initialization
     while True:
+
         if currentMode == modeType.Initializing:
-            print(modeType.Initialization.name)      # print name of enumeration
-            oledDriver.printToOled(modeType.Initialization.name)
+            # print(modeType.Initializing.name)      # print name of enumeration
+            #oledDriver.printToOled(modeType.Initialization.name)
             currentMode = modeType.Profile_Select
         elif currentMode == modeType.Profile_Select:
-            print(modeType.Profile_Select.name)
-            oledDriver.printToOled(modeType.Profile_Select.name)
+            # print(modeType.Profile_Select.name)
+            #oledDriver.printToOled(modeType.Profile_Select.name)
             if keyboard.is_pressed('t'):            # trained
                 currentMode = modeType.Live_Mode
             if keyboard.is_pressed('u'):            # untrained
                 currentMode = modeType.Training_Mode
         elif currentMode == modeType.Training_Mode:
-            print(modeType.Training_Mode.name)
-            oledDriver.printToOled(modeType.Training_Mode.name)
+            # print(modeType.Training_Mode.name)
+            #oledDriver.printToOled(modeType.Training_Mode.name)
             currentMode = modeType.Live_Mode
-        elif currentMode == modeType.Live_Mode:
-            print(modeType.Live_Mode.name)
-            oledDriver.printToOled(modeType.Live_Mode.name)
+        #elif currentMode == modeType.Live_Mode:
+            # print(modeType.Live_Mode.name)
+            #oledDriver.printToOled(modeType.Live_Mode.name)
+        if previousMode != currentMode:
+            print(currentMode.name)
+        previousMode = currentMode
 
 
+
+if __name__ == '__main__':
+    main()
