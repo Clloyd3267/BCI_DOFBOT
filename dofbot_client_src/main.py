553b8e3e937480b55dc36d5aa259fdd27a19fc49
dshuhart <shuhardc@clarkson.edu>
2021-11-16 15:22:33 -0500

    Update main.py

--- dofbot_client_src/main.py
+++ dofbot_client_src/main.py
@@ -2,6 +2,7 @@ import enum
 import time
 from ClientInterfaceDriver import *
 from SmartSockets.SmartSocket import *
+import keyboard
 
 #from oledDriver import *
 
@@ -25,40 +26,45 @@ def keyboardPluggedIn():
 
 
 def main():
-    previousMode = None
-    currentMode = modeType.Initializing  # set default mode to Initialization
-    #oledDriver = Draw()
-    while True:
+	previousMode = None
+	currentMode = modeType.Initializing  # set default mode to Initialization
+	#oledDriver = Draw()
+	while True:
+		
+		if currentMode == modeType.Initializing:
+			print(modeType.Initializing.name)      # print name of enumeration
+			#oledDriver.printToOled(modeType.Initializing.name)
+			currentMode = modeType.Profile_Select
+		elif currentMode == modeType.Profile_Select:
+			createProfile = server.createProfile()
+			profileList = server.listProfiles()
+			
+			#print("--------Select or Create a Profile---------")
+			userInput = input("Type Select or Create")
 
-        if currentMode == modeType.Initializing:
-            #print(modeType.Initializing.name)      # print name of enumeration
-            #oledDriver.printToOled(modeType.Initializing.name)
-            currentMode = modeType.Profile_Select
-        elif currentMode == modeType.Profile_Select:
-            createProfile = server.createProfile()
-            profileList = server.listProfiles()
+			if userInput == select:
+				print("Current Profile List:")
+				print("Index | Profile Name")
+				for index, value in enumerate(profileList):
+					print ("{} | {}".format(index, value))
 
 
-            print("--------Select or Create a Profile---------")
-            print("Current Profile List")
-
-
-            # print(modeType.Profile_Select.name)
-            #oledDriver.printToOled(modeType.Profile_Select.name)
-            #if input('t'):            # trained
-                #currentMode = modeType.Live_Mode
-            #if input('u'):            # untrained
-                #currentMode = modeType.Training_Mode
-        elif currentMode == modeType.Training_Mode:
-            #print(modeType.Training_Mode.name)
-            #oledDriver.printToOled(modeType.Training_Mode.name)
-            currentMode = modeType.Live_Mode
-        # elif currentMode == modeType.Live_Mode:
-        # print(modeType.Live_Mode.name)
-        # oledDriver.printToOled(modeType.Live_Mode.name)
-        if previousMode != currentMode:
-            print(currentMode.name)
-        previousMode = currentMode
+			# print(modeType.Profile_Select.name)
+			#oledDriver.printToOled(modeType.Profile_Select.name)
+			#if input('t'):            # trained
+				#currentMode = modeType.Live_Mode
+			#if input('u'):            # untrained
+				#currentMode = modeType.Training_Mode
+		elif currentMode == modeType.Training_Mode:
+			#print(modeType.Training_Mode.name)
+			#oledDriver.printToOled(modeType.Training_Mode.name)
+			currentMode = modeType.Live_Mode
+		# elif currentMode == modeType.Live_Mode:
+		# print(modeType.Live_Mode.name)
+		# oledDriver.printToOled(modeType.Live_Mode.name)
+		if previousMode != currentMode:
+			print(currentMode.name)
+		previousMode = currentMode
 
 
 if __name__ == '__main__':
