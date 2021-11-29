8a4d061f111a7be62bbb4fefcf97a9d3a068da7f
Josiah Schmidt <37901964+jnschmidt18@users.noreply.github.com>
2021-11-29 15:43:48 -0500

    Fully working

--- dofbot_client_src/IODriver.py
+++ dofbot_client_src/IODriver.py
@@ -6,11 +6,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 BTN_A_PIN=40
-BTN_B_PIN=11
-BTN_C_PIN=13
-BTN_D_PIN=15
-BTN_E_PIN=29
-BTN_F_PIN=31
+BTN_B_PIN=38
+BTN_C_PIN=36
+BTN_D_PIN=32
+BTN_E_PIN=26
+BTN_F_PIN=24
 
 GPIO.setup(BTN_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BTN_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -20,35 +20,29 @@ GPIO.setup(BTN_E_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BTN_F_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def signal_handler(sig, frame): #close GPIOs on sigint
-	GPIO.cleanup()
-	sys.exit(0)
+        GPIO.cleanup()
+        sys.exit(0)
 
 def Poll_A(): #holds thread until button A is pushed
-	GPIO.wait_for_edge(BTN_A_PIN, GPIO.RISING)
-	return True
+        GPIO.wait_for_edge(BTN_A_PIN, GPIO.RISING)
 
 def Poll_B(): #holds thread until button B is pushed
         GPIO.wait_for_edge(BTN_B_PIN, GPIO.RISING)
-        return True
 
 def Poll_C(): #holds thread until button C is pushed
         GPIO.wait_for_edge(BTN_C_PIN, GPIO.RISING)
-        return True
 
 def Poll_D(): #holds thread until button D is pushed
         GPIO.wait_for_edge(BTN_D_PIN, GPIO.RISING)
-        return True
 
 def Poll_E(): #holds thread until button E is pushed
         GPIO.wait_for_edge(BTN_E_PIN, GPIO.RISING)
-        return True
 
 def Poll_F(): #holds thread until button F is pushed
         GPIO.wait_for_edge(BTN_F_PIN, GPIO.RISING)
-        return True
 
 def interruption(pin):
-	print("interrupted at " + str(pin))
+        print("interrupted at " + str(pin))
 
 def interrupt_A(): #will return true once A is pushed
 	GPIO.add_event_detect(BTN_A_PIN, GPIO.FALLING, callback = interruption, bouncetime=100)
