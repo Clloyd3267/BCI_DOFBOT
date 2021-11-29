e1200f20dc0d240c2521dcf5f5d93b061b53798e
dshuhart <shuhardc@clarkson.edu>
2021-11-29 15:10:53 -0500

    Create UserIOInteraction.py

---
+++ dofbot_client_src/UserIOInteraction.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# Name         : UserIOInteraction.py
+# Date Created : 11/4/2021
+# Author(s)    : Chris Lloyd, Josiah Schmidt, Camilla Ketola, Dylan Shuhart
+# Description  : CDL=> Here
+# ------------------------------------------------------------------------------
+
+#Imports
+
+def printMessage(message):
+    print(message)
+def promptUserInput(message):
+    return input(message)
+def promptUserList(message, list):
+    pass
